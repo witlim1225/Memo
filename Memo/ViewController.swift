717cf74f4b246a008907e2650a9e9d28021cede4
witlim1225 <witlim1225@gmail.com>
2022-04-19 09:21:15 +0900

    Initial Commit

---
+++ Memo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Memo
+//
+//  Created by TWLim on 2022/04/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
